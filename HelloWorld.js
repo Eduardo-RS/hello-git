738bd29c4e9900c3de030e71eef1835966678299
Eduardo RS <EduardoRS2195@gmail.com>
2025-01-16 11:04:51 -0800

    Device Class Has been Added

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,9 @@
 console.log('Hello World 2');
+
+class Devices {
+    constructor(deviceName, price, quantity){
+        this.deviceName = deviceName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
