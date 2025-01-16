15a80d4c230f43cea9417934e7e40dea19ca58b1
Eduardo RS <EduardoRS2195@gmail.com>
2025-01-16 11:24:23 -0800

    An instance and method from Inventory has been created

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,9 +1,14 @@
 console.log('Hello World 2');
 
-class Devices {
+class Inventory {
     constructor(deviceName, price, quantity){
         this.deviceName = deviceName;
         this.price = price;
         this.quantity = quantity;
     }
+    show(){
+        return this.deviceName, this.price, this.quantity;
+    }
 }
+
+const newDevice = new Devices('Macbook Pro', 900, 20)
